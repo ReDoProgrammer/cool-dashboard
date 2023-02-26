4667a3048be45fd7307278d87894dd356b6dd3f2
redoprogrammer <redoprogrammer@gmail.com>
2023-02-26 16:59:28 +0700

    error useNavigate when set route

--- src/components/AppRoutes/index.js
+++ src/components/AppRoutes/index.js
@@ -1,4 +1,16 @@
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import {Dashboard} from '../../pages/Dashboard';
+import {Inventory} from '../../pages/Inventory';
+import {Orders} from '../../pages/Orders';
+import {Customers} from '../../pages/Customers';
 function AppRoutes(){
-
+    <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<Dashboard/>}></Route>
+        <Route path='/inventory' element={<Inventory/>}></Route>
+        <Route path='/orders' element={<Orders/>}></Route>
+        <Route path='/customers' element={<Customers/>}></Route>
+    </Routes>
+    </BrowserRouter>
 }
 export default AppRoutes;
