9e62dba96dbc0cc591278a0f16bd4700c3f3c2a4
redoprogrammer <redoprogrammer@gmail.com>
2023-02-26 15:48:26 +0700

    define components and import them

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import {} from 'antd';
+import { Space } from 'antd';
 import './App.css';
-
+import AppHeader from './components/AppHeader';
+import AppFooter from './components/AppFooter';
+import PageContent from './components/PageContent';
+import SideMenu from './components/SideMenu';
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <AppHeader/>
+      <Space>
+        <SideMenu></SideMenu>
+        <PageContent></PageContent>
+      </Space>
+      <AppFooter/>
+    </div>
   );
 }
 
---
+++ src/components/PageContent/index.js
@@ -0,0 +1,5 @@
+function PageContent(){
+    return <div>Page Content</div>
+}
+
+export default PageContent;
